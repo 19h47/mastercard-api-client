ec20356fab1b5de018209ce591e95aef1e898f75
19h47 <jeremylevron@19h47.fr>
2020-10-22 12:17:28 +0200

    :sparkles: Bundle with Webpack

---
+++ webpack/webpack.config.common.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * @file webpack.config.common.js
+ * @author Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ */
+
+// Plugins
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WebpackNotifierPlugin = require("webpack-notifier");
+
+const resolve = require("./webpack.utils");
+
+module.exports = {
+	entry: {
+		dist: resolve("src/index.js"),
+	},
+	output: {
+		library: "MastercardApiClient",
+		libraryTarget: "umd",
+		filename: "../[name]/main.js",
+	},
+	devServer: {
+		contentBase: resolve("/"),
+		compress: true,
+		port: 3000,
+		inline: true,
+		disableHostCheck: true,
+		writeToDisk: true,
+	},
+	resolve: {
+		alias: {
+			"@": resolve("src"),
+			Utils: resolve("src/utils"),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin({
+			cleanOnceBeforeBuildPatterns: [resolve("dist")],
+		}),
+		new WebpackNotifierPlugin({
+			title: "Webpack",
+			excludeWarnings: true,
+			alwaysNotify: true,
+		}),
+	],
+};
